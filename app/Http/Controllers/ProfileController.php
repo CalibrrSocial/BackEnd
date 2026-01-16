e99c707ecff750126bbf79e28ba569040af59d95
CalibrrSocial <nolanbias99@gmail.com>
2026-01-16 12:00:00 -0500

    Add eliyoung4now@gmail.com back to report notification email recipients

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2247,7 +2247,7 @@ class ProfileController extends Controller
     private function sendReportNotificationEmails($report)
     {
         try {
-            $adminEmails = ['nolan@calibrr.com', 'contact@calibrr.com'];
+            $adminEmails = ['nolan@calibrr.com', 'contact@calibrr.com', 'eliyoung4now@gmail.com'];
             $reasonName = $report->reason_category; // User-typed reason
             
             $emailData = [
